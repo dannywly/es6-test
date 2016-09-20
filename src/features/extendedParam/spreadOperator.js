f2c019c5cf9deda0c9bd6d6f421c27a1b6981ba6
danny <1208673450@qq.com>
2016-09-20 22:49:54 +0800

    ES6(grunt-babel): add some test case of ES6 feature

---
+++ src/features/extendedParam/spreadOperator.js
@@ -0,0 +1,10 @@
+function f (x, y, ...a) {
+    return (x + y) * a.length;
+}
+
+var params = [ "hello", true, 7 ];
+var other = [ 1, 2, ...params ]; // [ 1, 2, "hello", true, 7 ]
+f(1, 2, ...params) === 9;
+
+var str = "foo";
+var chars = [ ...str ]; // [ "f", "o", "o" ]
